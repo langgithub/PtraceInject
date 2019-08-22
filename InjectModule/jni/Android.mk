463c022863d733fb380ac550edad13119048e781
袁浪 <yuanlang@bigsfintech.com>
2019-08-22 18:06:01 +0800

    this

--- InjectModule/jni/Android.mk
+++ InjectModule/jni/Android.mk
@@ -6,6 +6,6 @@ LOCAL_SRC_FILES := InjectModule.c
 
 LOCAL_ARM_MODE := arm
   
-LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog  
+LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog 
   
 include $(BUILD_SHARED_LIBRARY)  
